1d8c3466129bcfe76aab3423e1ab8817654f3d6d
John Olawale <mojoblanco@gmail.com>
2018-08-24 19:28:30 +0100

    Refactor passport config

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
+var passport = require('../config/passport');
 var User = require('../models/User');
-
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.getUserByUsername(username, function(err, user) {
-            if (err) throw err;
-            if (!user) {
-                return done(null, false, { message: 'Unknown User' });
-            }
-
-            User.comparePassword(password, user.password, function(err, isMatch) {
-                if (err) throw err;
-                if (isMatch) {
-                    return done(null, user);
-                } else {
-                    return done(null, false, { message: 'Invalid password' });
-                }
-            });
-        });
-    }));
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.getUserById(id, function(err, user) {
-        done(err, user);
-    });
-});
 
 // Login
 router.get('/login', function(req, res) {
