63a7fb8194416a2a6610f8bdc77b7e7a8bb59bb3
John Olawale <mojoblanco@gmail.com>
2018-08-30 21:06:06 +0100

    Switch to mdbootstrap

--- app.js
+++ app.js
@@ -43,9 +43,11 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/mdbootstrap/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/mdbootstrap/js')));
 
 app.use(session({
     secret: 'my-secret',
